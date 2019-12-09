9831c18d5a2960546d39648f433e9e2b06451c81
Rostyslav Gress <nexusskv@i.ua>
2019-12-09 17:01:45 +0200

    - Created the architecture for project.

---
+++ BestGoalsClub/BestGoalsClub/BestGoalsClub/Modules/Companies/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BestGoalsClub
+//
+//  Created by Rost on 09.12.2019.
+//  Copyright © 2019 Rost Gress. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
